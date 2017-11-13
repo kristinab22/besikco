6c6e782d0dc71252a0c0ba3f7d91b804c472e801
KB <betheakristina@gmail.com>
2017-11-12 21:53:00 -0800

    fixed rwd shopify layout on hat page; added new images to home page

--- besikco.js
+++ besikco.js
@@ -1,4 +1,3 @@
-// bésik navy blue trucket hat
 /*<![CDATA[*/
   (function () {
     var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
@@ -29,8 +28,8 @@
 
     ShopifyBuy.UI.onReady(client).then(function (ui) {
       ui.createComponent('product', {
-        id: [365981532198],
-        node: document.getElementById('product-component-1510197046732'),
+        id: [384309559334],
+        node: document.getElementById('product-component-1510548358416'),
         moneyFormat: '${{amount}}',
         options: {"product":{"variantId":"all","width":"240px","contents":{"imgWithCarousel":false,"variantTitle":false,"description":false,"buttonWithQuantity":false,"quantity":false},"styles":{"product":{"@media (min-width: 601px)":{"max-width":"100%","margin-left":"0","margin-bottom":"50px"}},"button":{"background-color":"#0ea2ce",":hover":{"background-color":"#0d92b9"},":focus":{"background-color":"#0d92b9"}},"compareAt":{"font-size":"12px"}}},"cart":{"contents":{"button":true},"text":{"title":"Your Shopping Cart","total":"TOTAL","notice":"Tax and discounts are included with purchase."},"styles":{"button":{"background-color":"#0ea2ce",":hover":{"background-color":"#0d92b9"},":focus":{"background-color":"#0d92b9"}},"footer":{"background-color":"#ffffff"}}},"modalProduct":{"contents":{"img":false,"imgWithCarousel":true,"variantTitle":false,"buttonWithQuantity":true,"button":false,"quantity":false},"styles":{"product":{"@media (min-width: 601px)":{"max-width":"100%","margin-left":"0px","margin-bottom":"0px"}},"button":{"background-color":"#0ea2ce",":hover":{"background-color":"#0d92b9"},":focus":{"background-color":"#0d92b9"}}}},"toggle":{"styles":{"toggle":{"background-color":"#0ea2ce",":hover":{"background-color":"#0d92b9"},":focus":{"background-color":"#0d92b9"}}}},"productSet":{"styles":{"products":{"@media (min-width: 601px)":{"margin-left":"-20px"}}}}},
       });
@@ -39,7 +38,6 @@
 })();
 /*]]>*/
 
-// bésik black trucker hat
 /*<![CDATA[*/
   (function () {
     var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
@@ -70,8 +68,8 @@
 
     ShopifyBuy.UI.onReady(client).then(function (ui) {
       ui.createComponent('product', {
-        id: [365990608934],
-        node: document.getElementById('product-component-1510197268336'),
+        id: [384318767142],
+        node: document.getElementById('product-component-1510548846461'),
         moneyFormat: '${{amount}}',
         options: {"product":{"variantId":"all","width":"240px","contents":{"imgWithCarousel":false,"variantTitle":false,"description":false,"buttonWithQuantity":false,"quantity":false},"styles":{"product":{"@media (min-width: 601px)":{"max-width":"100%","margin-left":"0","margin-bottom":"50px"}},"button":{"background-color":"#0ea2ce",":hover":{"background-color":"#0d92b9"},":focus":{"background-color":"#0d92b9"}},"compareAt":{"font-size":"12px"}}},"cart":{"contents":{"button":true},"text":{"title":"Your Shopping Cart","total":"TOTAL","notice":"Tax and discounts are included with purchase."},"styles":{"button":{"background-color":"#0ea2ce",":hover":{"background-color":"#0d92b9"},":focus":{"background-color":"#0d92b9"}},"footer":{"background-color":"#ffffff"}}},"modalProduct":{"contents":{"img":false,"imgWithCarousel":true,"variantTitle":false,"buttonWithQuantity":true,"button":false,"quantity":false},"styles":{"product":{"@media (min-width: 601px)":{"max-width":"100%","margin-left":"0px","margin-bottom":"0px"}},"button":{"background-color":"#0ea2ce",":hover":{"background-color":"#0d92b9"},":focus":{"background-color":"#0d92b9"}}}},"toggle":{"styles":{"toggle":{"background-color":"#0ea2ce",":hover":{"background-color":"#0d92b9"},":focus":{"background-color":"#0d92b9"}}}},"productSet":{"styles":{"products":{"@media (min-width: 601px)":{"margin-left":"-20px"}}}}},
       });
@@ -80,7 +78,7 @@
 })();
 /*]]>*/
 
-//bésik white snapback - all blue logo
+// besik white hat - blue logo
 /*<![CDATA[*/
   (function () {
     var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
@@ -111,8 +109,49 @@
 
     ShopifyBuy.UI.onReady(client).then(function (ui) {
       ui.createComponent('product', {
-        id: [366001717286],
-        node: document.getElementById('product-component-1510197467410'),
+        id: [384327745574],
+        node: document.getElementById('product-component-1510549504693'),
+        moneyFormat: '${{amount}}',
+        options: {"product":{"variantId":"all","width":"240px","contents":{"imgWithCarousel":false,"variantTitle":false,"description":false,"buttonWithQuantity":false,"quantity":false},"styles":{"product":{"@media (min-width: 601px)":{"max-width":"100%","margin-left":"0","margin-bottom":"50px"}},"button":{"background-color":"#0ea2ce",":hover":{"background-color":"#0d92b9"},":focus":{"background-color":"#0d92b9"}},"compareAt":{"font-size":"12px"}}},"cart":{"contents":{"button":true},"text":{"title":"Your Shopping Cart","total":"TOTAL","notice":"Tax and discounts are included with purchase."},"styles":{"button":{"background-color":"#0ea2ce",":hover":{"background-color":"#0d92b9"},":focus":{"background-color":"#0d92b9"}},"footer":{"background-color":"#ffffff"}}},"modalProduct":{"contents":{"img":false,"imgWithCarousel":true,"variantTitle":false,"buttonWithQuantity":true,"button":false,"quantity":false},"styles":{"product":{"@media (min-width: 601px)":{"max-width":"100%","margin-left":"0px","margin-bottom":"0px"}},"button":{"background-color":"#0ea2ce",":hover":{"background-color":"#0d92b9"},":focus":{"background-color":"#0d92b9"}}}},"toggle":{"styles":{"toggle":{"background-color":"#0ea2ce",":hover":{"background-color":"#0d92b9"},":focus":{"background-color":"#0d92b9"}}}},"productSet":{"styles":{"products":{"@media (min-width: 601px)":{"margin-left":"-20px"}}}}},
+      });
+    });
+  }
+})();
+/*]]>*/
+
+// besik original - red
+/*<![CDATA[*/
+  (function () {
+    var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
+    if (window.ShopifyBuy) {
+      if (window.ShopifyBuy.UI) {
+        ShopifyBuyInit();
+      } else {
+        loadScript();
+      }
+    } else {
+      loadScript();
+    }
+
+  function loadScript() {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = scriptURL;
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+    script.onload = ShopifyBuyInit;
+  }
+
+  function ShopifyBuyInit() {
+    var client = ShopifyBuy.buildClient({
+      domain: 'besik.myshopify.com',
+      apiKey: 'b48d63181663503a28908895459f7bd4',
+      appId: '6',
+    });
+
+    ShopifyBuy.UI.onReady(client).then(function (ui) {
+      ui.createComponent('product', {
+        id: [384367722534],
+        node: document.getElementById('product-component-1510551797642'),
         moneyFormat: '${{amount}}',
         options: {"product":{"variantId":"all","width":"240px","contents":{"imgWithCarousel":false,"variantTitle":false,"description":false,"buttonWithQuantity":false,"quantity":false},"styles":{"product":{"@media (min-width: 601px)":{"max-width":"100%","margin-left":"0","margin-bottom":"50px"}},"button":{"background-color":"#0ea2ce",":hover":{"background-color":"#0d92b9"},":focus":{"background-color":"#0d92b9"}},"compareAt":{"font-size":"12px"}}},"cart":{"contents":{"button":true},"text":{"title":"Your Shopping Cart","total":"TOTAL","notice":"Tax and discounts are included with purchase."},"styles":{"button":{"background-color":"#0ea2ce",":hover":{"background-color":"#0d92b9"},":focus":{"background-color":"#0d92b9"}},"footer":{"background-color":"#ffffff"}}},"modalProduct":{"contents":{"img":false,"imgWithCarousel":true,"variantTitle":false,"buttonWithQuantity":true,"button":false,"quantity":false},"styles":{"product":{"@media (min-width: 601px)":{"max-width":"100%","margin-left":"0px","margin-bottom":"0px"}},"button":{"background-color":"#0ea2ce",":hover":{"background-color":"#0d92b9"},":focus":{"background-color":"#0d92b9"}}}},"toggle":{"styles":{"toggle":{"background-color":"#0ea2ce",":hover":{"background-color":"#0d92b9"},":focus":{"background-color":"#0d92b9"}}}},"productSet":{"styles":{"products":{"@media (min-width: 601px)":{"margin-left":"-20px"}}}}},
       });
@@ -154,47 +193,6 @@
        ui.createComponent('product', {
          id: [366012497958],
          node: document.getElementById('product-component-1510197753006'),
-         moneyFormat: '${{amount}}',
-         options: {"product":{"variantId":"all","width":"240px","contents":{"imgWithCarousel":false,"variantTitle":false,"description":false,"buttonWithQuantity":false,"quantity":false},"styles":{"product":{"@media (min-width: 601px)":{"max-width":"100%","margin-left":"0","margin-bottom":"50px"}},"button":{"background-color":"#0ea2ce",":hover":{"background-color":"#0d92b9"},":focus":{"background-color":"#0d92b9"}},"compareAt":{"font-size":"12px"}}},"cart":{"contents":{"button":true},"text":{"title":"Your Shopping Cart","total":"TOTAL","notice":"Tax and discounts are included with purchase."},"styles":{"button":{"background-color":"#0ea2ce",":hover":{"background-color":"#0d92b9"},":focus":{"background-color":"#0d92b9"}},"footer":{"background-color":"#ffffff"}}},"modalProduct":{"contents":{"img":false,"imgWithCarousel":true,"variantTitle":false,"buttonWithQuantity":true,"button":false,"quantity":false},"styles":{"product":{"@media (min-width: 601px)":{"max-width":"100%","margin-left":"0px","margin-bottom":"0px"}},"button":{"background-color":"#0ea2ce",":hover":{"background-color":"#0d92b9"},":focus":{"background-color":"#0d92b9"}}}},"toggle":{"styles":{"toggle":{"background-color":"#0ea2ce",":hover":{"background-color":"#0d92b9"},":focus":{"background-color":"#0d92b9"}}}},"productSet":{"styles":{"products":{"@media (min-width: 601px)":{"margin-left":"-20px"}}}}},
-       });
-     });
-   }
- })();
- /*]]>*/
-
- //bésijk white snapback - all red logo
- /*<![CDATA[*/
-   (function () {
-     var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
-     if (window.ShopifyBuy) {
-       if (window.ShopifyBuy.UI) {
-         ShopifyBuyInit();
-       } else {
-         loadScript();
-       }
-     } else {
-       loadScript();
-     }
-
-   function loadScript() {
-     var script = document.createElement('script');
-     script.async = true;
-     script.src = scriptURL;
-     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
-     script.onload = ShopifyBuyInit;
-   }
-
-   function ShopifyBuyInit() {
-     var client = ShopifyBuy.buildClient({
-       domain: 'besik.myshopify.com',
-       apiKey: 'b48d63181663503a28908895459f7bd4',
-       appId: '6',
-     });
-
-     ShopifyBuy.UI.onReady(client).then(function (ui) {
-       ui.createComponent('product', {
-         id: [366031765542],
-         node: document.getElementById('product-component-1510198193509'),
          moneyFormat: '${{amount}}',
          options: {"product":{"variantId":"all","width":"240px","contents":{"imgWithCarousel":false,"variantTitle":false,"description":false,"buttonWithQuantity":false,"quantity":false},"styles":{"product":{"@media (min-width: 601px)":{"max-width":"100%","margin-left":"0","margin-bottom":"50px"}},"button":{"background-color":"#0ea2ce",":hover":{"background-color":"#0d92b9"},":focus":{"background-color":"#0d92b9"}},"compareAt":{"font-size":"12px"}}},"cart":{"contents":{"button":true},"text":{"title":"Your Shopping Cart","total":"TOTAL","notice":"Tax and discounts are included with purchase."},"styles":{"button":{"background-color":"#0ea2ce",":hover":{"background-color":"#0d92b9"},":focus":{"background-color":"#0d92b9"}},"footer":{"background-color":"#ffffff"}}},"modalProduct":{"contents":{"img":false,"imgWithCarousel":true,"variantTitle":false,"buttonWithQuantity":true,"button":false,"quantity":false},"styles":{"product":{"@media (min-width: 601px)":{"max-width":"100%","margin-left":"0px","margin-bottom":"0px"}},"button":{"background-color":"#0ea2ce",":hover":{"background-color":"#0d92b9"},":focus":{"background-color":"#0d92b9"}}}},"toggle":{"styles":{"toggle":{"background-color":"#0ea2ce",":hover":{"background-color":"#0d92b9"},":focus":{"background-color":"#0d92b9"}}}},"productSet":{"styles":{"products":{"@media (min-width: 601px)":{"margin-left":"-20px"}}}}},
        });
